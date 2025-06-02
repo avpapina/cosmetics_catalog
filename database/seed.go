ec3117441e11dd29852fe1f6c9096a37819c271e
Тихонова Елизавета Олеговна <eotikhonova@edu.hse.ru>
2025-06-02 14:49:00 +0300

    обновила бд

--- database/seed.go
+++ database/seed.go
@@ -1,22 +1,32 @@
 package database
 
-import "cosmetics_catalog/models"
+import (
+	"cosmetics_catalog/models"
+)
 
 func SeedTestData() error {
-	// Создание брендов
+	// 1. Бренды
 	brands := []models.Brand{
-		{Name: "L'Oreal", Slug: "loreal"},
-		{Name: "Maybelline", Slug: "maybelline"},
-		{Name: "Nivea", Slug: "nivea"},
+		{Name: "Bioderma", Slug: "bioderma"},
+		{Name: "L'Oréal Paris", Slug: "loreal-paris"},
+		{Name: "Pusy", Slug: "pusy"},
+		{Name: "Dior", Slug: "dior"},
+		{Name: "Dr. Jart+", Slug: "dr-jart"},
+		{Name: "Clarins", Slug: "clarins"},
+		{Name: "Estée Lauder", Slug: "estee-lauder"},
+		{Name: "Catrice", Slug: "catrice"},
+		{Name: "Clinique", Slug: "clinique"},
+		{Name: "Shik", Slug: "shik"},
+		{Name: "Kiko Milano", Slug: "kiko-milano"},
+		{Name: "Erborian", Slug: "erborian"},
 	}
-
 	for _, brand := range brands {
 		if err := DB.Create(&brand).Error; err != nil {
 			return err
 		}
 	}
 
-	// Создание категорий и подкатегорий
+	// 2. Категории с подкатегориями
 	categories := []models.Category{
 		{
 			Name: "Макияж",
@@ -33,25 +43,42 @@ func SeedTestData() error {
 			Subcategories: []models.Subcategory{
 				{Name: "Очищение", Slug: "ochishenie"},
 				{Name: "Увлажнение", Slug: "uvlazhnenie"},
-				{Name: "Маски", Slug: "maski"},
+				{Name: "Тонизирование", Slug: "tonizirovanie"},
 			},
 		},
 	}
-
 	for _, category := range categories {
 		if err := DB.Create(&category).Error; err != nil {
 			return err
 		}
 	}
 
-	// Создание тестовых продуктов
-	// Создание тестовых продуктов
+	// 3. Получение нужных подкатегорий из БД
+	var (
+		makeupFace, makeupEyes, makeupLips       models.Subcategory
+		careCleansing, careHydration, careToning models.Subcategory
+	)
+	subMap := map[string]*models.Subcategory{
+		"Лицо":          &makeupFace,
+		"Глаза":         &makeupEyes,
+		"Губы":          &makeupLips,
+		"Очищение":      &careCleansing,
+		"Увлажнение":    &careHydration,
+		"Тонизирование": &careToning,
+	}
+	for name, ptr := range subMap {
+		if err := DB.Where("name = ?", name).First(ptr).Error; err != nil {
+			return err
+		}
+	}
+
+	// 4. Продукты
 	products := []models.Product{
 		{
 			Name:          "Тональный крем",
 			Slug:          "tonalnyi-krem",
 			BrandID:       1,
-			SubcategoryID: 1, // Лицо
+			SubcategoryID: makeupFace.ID,
 			Price:         1299.99,
 			ImagePath:     "images/creme.jpg",
 			Description:   "Легкий тональный крем с натуральным покрытием",
@@ -60,7 +87,7 @@ func SeedTestData() error {
 			Name:          "Увлажняющий крем",
 			Slug:          "uvlazhnyayushchii-krem",
 			BrandID:       1,
-			SubcategoryID: 1, // Лицо
+			SubcategoryID: careHydration.ID,
 			Price:         1499.00,
 			ImagePath:     "images/moisturizer.jpg",
 			Description:   "Интенсивное увлажнение на 24 часа",
@@ -71,7 +98,7 @@ func SeedTestData() error {
 			Name:          "Очищающая пенка",
 			Slug:          "ochishchayushchaya-penka",
 			BrandID:       3,
-			SubcategoryID: 1, // Лицо
+			SubcategoryID: careCleansing.ID,
 			Price:         799.50,
 			ImagePath:     "images/cleanser.jpg",
 			Description:   "Мягкое очищение без стягивания",
@@ -80,7 +107,7 @@ func SeedTestData() error {
 			Name:          "Сыворотка с гиалуроновой кислотой",
 			Slug:          "syvorotka-s-gialuronovoi-kislotoy",
 			BrandID:       2,
-			SubcategoryID: 1, // Лицо
+			SubcategoryID: careHydration.ID,
 			Price:         2499.00,
 			ImagePath:     "images/serum.jpg",
 			Description:   "Глубокое увлажнение и разглаживание морщин",
@@ -89,7 +116,7 @@ func SeedTestData() error {
 			Name:          "BB-крем",
 			Slug:          "bb-krem",
 			BrandID:       4,
-			SubcategoryID: 1, // Лицо
+			SubcategoryID: makeupFace.ID,
 			Price:         1599.00,
 			ImagePath:     "images/bb-cream.jpg",
 			Description:   "Многофункциональный уход и макияж в одном",
@@ -100,15 +127,111 @@ func SeedTestData() error {
 			Name:          "Тушь для ресниц",
 			Slug:          "tush-dlya-resnits",
 			BrandID:       2,
-			SubcategoryID: 2, // Глаза
+			SubcategoryID: makeupEyes.ID,
 			Price:         899.99,
 			ImagePath:     "images/mascara.jpg",
 			Description:   "Объемная тушь для ресниц",
 			IsOnSale:      true,
 			SalePrice:     699.99,
 		},
-	}
+		{
+			Name:          "Бальзам для губ",
+			Slug:          "balzam-dlya-gub",
+			BrandID:       5,
+			SubcategoryID: makeupLips.ID,
+			Price:         499.00,
+			ImagePath:     "images/lip-balm.jpg",
+			Description:   "Увлажняющий бальзам для губ",
+		},
+		{
+			Name:          "Тоник для лица",
+			Slug:          "tonik-dlya-litsa",
+			BrandID:       6,
+			SubcategoryID: careToning.ID,
+			Price:         899.00,
+			ImagePath:     "images/toner.jpg",
+			Description:   "Тонизирующее средство для свежести кожи",
+		},
+		// bioderma
+		{
+			Name:          "Гель для умывания Sensibio Foaming Gel",
+			Slug:          "bioderma-sensibio-foaming-gel",
+			BrandID:       1,
+			SubcategoryID: careCleansing.ID,
+			Price:         1450.00,
+			ImagePath:     "images/bioderma/sensibio-foaming-gel.jpg",
+			Description:   "Мягкий пенящийся гель для чувствительной кожи",
+			IsOnSale:      false,
+		},
+		{
+			Name:          "Мицеллярная вода Sensibio H2O",
+			Slug:          "bioderma-sensibio-h2o",
+			BrandID:       1,
+			SubcategoryID: careCleansing.ID,
+			Price:         1290.00,
+			ImagePath:     "images/bioderma/sensibio-h2o.jpg",
+			Description:   "Легендарная мицеллярная вода для чувствительной кожи",
+			IsOnSale:      true,
+			SalePrice:     1099.00,
+		},
+		// L'Oréal Paris
+		{
+			Name:          "Тональный крем True Match",
+			Slug:          "loreal-true-match",
+			BrandID:       2,
+			SubcategoryID: makeupFace.ID,
+			Price:         899.00,
+			ImagePath:     "images/loreal/true-match.jpg",
+			Description:   "Тональный крем с естественным покрытием",
+			IsOnSale:      false,
+		},
+		{
+			Name:          "Тушь для ресниц Lash Paradise",
+			Slug:          "loreal-lash-paradise",
+			BrandID:       2,
+			SubcategoryID: makeupEyes.ID,
+			Price:         799.00,
+			ImagePath:     "images/loreal/lash-paradise.jpg",
+			Description:   "Объемная тушь для эффекта накладных ресниц",
+			IsOnSale:      true,
+			SalePrice:     699.00,
+		},
 
+		// Dior
+		{
+			Name:          "Помада Dior Addict Lip Glow",
+			Slug:          "dior-lip-glow",
+			BrandID:       4,
+			SubcategoryID: makeupLips.ID,
+			Price:         3200.00,
+			ImagePath:     "images/dior/lip-glow.jpg",
+			Description:   "Бальзам для губ с эффектом сияния",
+			IsOnSale:      false,
+		},
+		{
+			Name:          "Тушь для ресниц Diorshow",
+			Slug:          "dior-diorshow",
+			BrandID:       4,
+			SubcategoryID: makeupEyes.ID,
+			Price:         2900.00,
+			ImagePath:     "images/dior/diorshow.jpg",
+			Description:   "Культовая тушь для ресниц",
+			IsOnSale:      true,
+			SalePrice:     2500.00,
+		},
+
+		// Dr. Jart+
+		{
+			Name:          "BB-крем Premium Beauty Balm",
+			Slug:          "drjart-bb-cream",
+			BrandID:       5,
+			SubcategoryID: makeupFace.ID,
+			Price:         2800.00,
+			ImagePath:     "images/drjart/bb-cream.jpg",
+			Description:   "Многофункциональный BB-крем с уходом",
+			IsOnSale:      false,
+		},
+	}
 	for _, product := range products {
 		if err := DB.Create(&product).Error; err != nil {
 			return err
