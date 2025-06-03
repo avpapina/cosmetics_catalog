5d183cbbf83ddef13837118525bf527dc3b9a2cc
avpap <avpapina@edu.hse.ru>
2025-06-04 01:41:27 +0300

    New version

--- main.go
+++ main.go
@@ -43,18 +43,34 @@ func handleCatalogRoutes(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimPrefix(r.URL.Path, "/catalog/")
 	parts := strings.Split(path, "/")
 
+	if len(parts) > 0 && (parts[0] == "favicon.ico" || parts[0] == "images" || parts[0] == "assets") {
+		http.NotFound(w, r)
+		return
+	}
+
 	switch {
-	case len(parts) == 0 || parts[0] == "":
+	case len(parts) == 0 || parts[0] == "": // /catalog/
 		handleMainPage(w, r)
-	case len(parts) == 1 && parts[0] != "" && parts[0] != "sales": // /
+
+	case len(parts) == 1 && parts[0] == "sales": // /catalog/sales
+		handleSaleProducts(w, r)
+
+	case len(parts) == 1 && parts[0] == "brands": // /catalog/brands
+		handleBrands(w, r)
+
+	case len(parts) == 1: // /catalog/{category}
 		handleCatalogSubcategory(w, r, parts[0])
-	case len(parts) == 1 && parts[0] != "" && parts[0] == "sales": // /catalog/sales/
-		handleCategoryProducts(w, r, parts[0], "")
-	case len(parts) == 2 && parts[0] != "sales": // /catalog/{category}
-		handleCategoryProducts(w, r, parts[0], parts[1])
-	case len(parts) == 2 && parts[0] == "sales" && parts[1] != "": // /catalog/sales/{product}
+
+	case len(parts) == 2 && parts[0] == "sales": // /catalog/sales/{product}
 		handleProduct(w, r, parts[0], "", parts[1])
-	case len(parts) == 3 && parts[1] != "" && parts[0] != "sales": // /catalog/{category}/{subcategory}/{product}
+
+	case len(parts) == 2 && parts[0] == "brands": // /catalog/brands/{brand}
+		handleBrandProducts(w, r, parts[1])
+
+	case len(parts) == 2: // /catalog/{category}/{subcategory}
+		handleCategoryProducts(w, r, parts[0], parts[1])
+
+	case len(parts) == 3 && parts[0] != "sales": // /catalog/{category}/{subcategory}/{product}
 		handleProduct(w, r, parts[0], parts[1], parts[2])
 
 	default:
@@ -115,6 +131,26 @@ func handleCatalogSubcategory(w http.ResponseWriter, r *http.Request, slug strin
 
 	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, "Ошибка рендеринга шаблона", http.StatusInternalServerError)
+	}
+}
+
+// Страница отображения брендов
+func handleBrands(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/brands.html")
+	if err != nil {
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
+	}
+
+	var brands []models.Brand
+	if err := database.DB.Find(&brands).Error; err != nil {
+		http.Error(w, "Ошибка получения категорий", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, brands)
+	if err != nil {
+		http.Error(w, "Ошибка рендеринга", http.StatusInternalServerError)
 	}
 }
 
@@ -211,20 +247,19 @@ func handleCategoryProducts(w http.ResponseWriter, r *http.Request, categorySlug
 	}
 
 	// Подготавливаем данные для шаблона
-	// Подготавливаем данные для шаблона
 	data := struct {
 		Filter          string
 		CategorySlug    string
 		SubcategorySlug string
-		MinPrice        float64 // Добавляем
-		MaxPrice        float64 // Добавляем
+		MinPrice        float64
+		MaxPrice        float64
 		Products        []models.Product
 	}{
 		Filter:          filter,
 		CategorySlug:    categorySlug,
 		SubcategorySlug: subcategorySlug,
-		MinPrice:        minPrice, // Добавляем
-		MaxPrice:        maxPrice, // Добавляем
+		MinPrice:        minPrice,
+		MaxPrice:        maxPrice,
 		Products:        subcat.Products,
 	}
 
@@ -250,12 +285,16 @@ func handleProduct(w http.ResponseWriter, r *http.Request, category, subcategory
 		Price       float64
 		ImagePath   string
 		Description string
+		IsOnSale    bool
+		SalePrice   float64
 	}{
 		Name:        product.Name,
 		Slug:        productSlug,
 		Price:       product.Price,
 		ImagePath:   product.ImagePath,
 		Description: product.Description,
+		IsOnSale:    product.IsOnSale,
+		SalePrice:   product.SalePrice,
 	}
 
 	tmpl, err := template.ParseFiles("templates/product.html")
@@ -268,5 +307,169 @@ func handleProduct(w http.ResponseWriter, r *http.Request, category, subcategory
 	// Рендерим шаблон
 	if err := tmpl.Execute(w, data); err != nil {
 		http.Error(w, "Ошибка рендеринга", http.StatusInternalServerError)
+	}
+}
+
+// Страница всех продуктов бренда
+func handleBrandProducts(w http.ResponseWriter, r *http.Request, brandSlug string) {
+	// Получаем параметры фильтрации
+	query := r.URL.Query()
+	filter := query.Get("filter")
+	if filter == "" {
+		filter = "no" // Значение по умолчанию
+	}
+
+	// Обработка ценового диапазона
+	var minPrice, maxPrice float64
+	if filter == "range" {
+		minPrice, _ = strconv.ParseFloat(query.Get("min_price"), 64)
+		maxPrice, _ = strconv.ParseFloat(query.Get("max_price"), 64)
+	}
+
+	var brand models.Brand
+	var products []models.Product
+	var err error
+
+	// Базовый запрос для получения бренда
+	baseQuery := database.DB.Where("slug = ?", strings.ToLower(brandSlug))
+
+	// В зависимости от фильтра применяем разные условия
+	switch filter {
+	case "no":
+		// Без фильтрации (все продукты бренда)
+		err = baseQuery.Preload("Products").First(&brand).Error
+		products = brand.Products
+
+	case "high":
+		// Сортировка по возрастанию цены
+		err = baseQuery.Preload("Products", func(db *gorm.DB) *gorm.DB {
+			return db.Order("products.price ASC")
+		}).First(&brand).Error
+		products = brand.Products
+
+	case "low":
+		// Сортировка по убыванию цены
+		err = baseQuery.Preload("Products", func(db *gorm.DB) *gorm.DB {
+			return db.Order("products.price DESC")
+		}).First(&brand).Error
+		products = brand.Products
+
+	case "range":
+		// Фильтрация по ценовому диапазону
+		err = baseQuery.Preload("Products", func(db *gorm.DB) *gorm.DB {
+			return db.
+				Where("products.price BETWEEN ? AND ?", minPrice, maxPrice)
+		}).First(&brand).Error
+		products = brand.Products
+
+	default:
+		http.NotFound(w, r)
+		return
+	}
+
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Загружаем шаблон
+	tmpl, err := template.ParseFiles("templates/products.html")
+	if err != nil {
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
+	}
+
+	// Подготавливаем данные для шаблона
+	data := struct {
+		Filter          string
+		CategorySlug    string
+		SubcategorySlug string
+		MinPrice        float64
+		MaxPrice        float64
+		Products        []models.Product
+	}{
+		Filter:          filter,
+		CategorySlug:    "brands",
+		SubcategorySlug: brandSlug,
+		MinPrice:        minPrice,
+		MaxPrice:        maxPrice,
+		Products:        products,
+	}
+
+	// Рендерим шаблон
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, "Ошибка рендеринга", http.StatusInternalServerError)
+	}
+}
+
+// Страница товаров со скидкой
+func handleSaleProducts(w http.ResponseWriter, r *http.Request) {
+
+	// Получаем параметры фильтрации
+	query := r.URL.Query()
+	filter := query.Get("filter")
+	if filter == "" {
+		filter = "no" // Значение по умолчанию
+	}
+
+	// Обработка ценового диапазона
+	var minPrice, maxPrice float64
+	if filter == "range" {
+		minPrice, _ = strconv.ParseFloat(query.Get("min_price"), 64)
+		maxPrice, _ = strconv.ParseFloat(query.Get("max_price"), 64)
+	}
+
+	var products []models.Product
+	var err error
+
+	// Создаем базовый запрос
+	dbQuery := database.DB.Model(&models.Product{}).Where("is_on_sale = true")
+
+	// Применяем фильтры
+	switch filter {
+	case "no":
+		// Без дополнительной фильтрации
+		err = dbQuery.Find(&products).Error
+	case "high":
+		// Сортировка по возрастанию цены
+		err = dbQuery.Order("price ASC").Find(&products).Error
+	case "low":
+		// Сортировка по убыванию цены
+		err = dbQuery.Order("price DESC").Find(&products).Error
+	case "range":
+		// Фильтрация по ценовому диапазону
+		err = dbQuery.
+			Where("price BETWEEN ? AND ?", minPrice, maxPrice).
+			Find(&products).Error
+	}
+
+	if err != nil {
+		http.Error(w, "Ошибка базы данных: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Загружаем шаблон
+	tmpl, err := template.ParseFiles("templates/products_sales.html")
+	if err != nil {
+		http.Error(w, "Ошибка загрузки шаблона", http.StatusInternalServerError)
+		return
+	}
+
+	// Подготавливаем данные для шаблона
+	data := struct {
+		Filter   string
+		MinPrice float64
+		MaxPrice float64
+		Products []models.Product
+	}{
+		Filter:   filter,
+		MinPrice: minPrice,
+		MaxPrice: maxPrice,
+		Products: products,
+	}
+
+	// Рендерим шаблон
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, "Ошибка рендеринга: "+err.Error(), http.StatusInternalServerError)
 	}
 }
